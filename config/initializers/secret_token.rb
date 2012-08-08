a9c189cc876b41d09baa508eac7b49c2c6325989
mj-jadhav <rajemayur.413@gmail.com>
2012-08-09 01:19:42 +0530

    Tadalist Application Basic GUI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TadalistClone::Application.config.secret_token = '6dec35f9b99428b2511f56b94b24577725543100b0790258de8f39e48dac2fb21613527006d6b6d233cf80a5a46025853df49b1a811ca5dfe91e8fc26fee9cea'
